c4487b98b6ed1893ce90c9ad22aadd906b57c9a0
Ondřej Kábele <126025302+F1shyyyy@users.noreply.github.com>
2023-10-31 22:22:00 +0100

    Made header fully functional

--- js/script.js
+++ js/script.js
@@ -1,19 +1,30 @@
-let clothingButton = document.querySelector("#clothing-button");
+let clothingButton = document.querySelector("#clth-button");
 let clothingDiv = document.querySelector("#clothing-dropdown");
-let sneakersButton = document.querySelector("#sneakers-button");
+let sneakersButton = document.querySelector("#snkrs-button");
 let sneakersDiv = document.querySelector("#sneakers-dropdown");
-let clthON = false;
-let snkrsON = false;
+let zapnutoClth = false;
+let zapnutoSnkrs = false;
 
 clothingButton.addEventListener("click", function(){
-    if (!clthON) {
-        clothingDiv.classList.add("show-clothing");
-        clthON = true;
+    clothingDiv.classList.toggle("show-clothing");
+    clothingDiv.classList.toggle("underline-clothing");
+    if (!zapnutoClth) {
+        document.documentElement.style.setProperty("--clth", "100%");
+        zapnutoClth = true;
     } else{
-        clothingDiv.removeAttribute("class");
-        clthON = false;
+        document.documentElement.style.setProperty("--clth", "0%");
+        zapnutoClth = false;
+
     }
 })
 sneakersButton.addEventListener("click", function(){
     sneakersDiv.classList.toggle("show-sneakers");
+    sneakersDiv.classList.toggle("underline-sneakers");
+    if (!zapnutoSnkrs) {
+        document.documentElement.style.setProperty("--snkrs", "100%");
+        zapnutoSnkrs = true;
+    } else{
+        document.documentElement.style.setProperty("--snkrs", "0%");
+        zapnutoSnkrs = false;
+    }
 })
