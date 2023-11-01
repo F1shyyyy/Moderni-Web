5a90ee6a48da20078f291d71576ec158923150a8
Ondřej Kábele <126025302+F1shyyyy@users.noreply.github.com>
2023-11-01 16:58:50 +0100

    Finished the website

--- js/script.js
+++ js/script.js
@@ -2,29 +2,19 @@ let clothingButton = document.querySelector("#clth-button");
 let clothingDiv = document.querySelector("#clothing-dropdown");
 let sneakersButton = document.querySelector("#snkrs-button");
 let sneakersDiv = document.querySelector("#sneakers-dropdown");
-let zapnutoClth = false;
-let zapnutoSnkrs = false;
+let jordanDivs = [document.querySelectorAll(".jordan-shoes")];
 
 clothingButton.addEventListener("click", function(){
     clothingDiv.classList.toggle("show-clothing");
     clothingDiv.classList.toggle("underline-clothing");
-    if (!zapnutoClth) {
-        document.documentElement.style.setProperty("--clth", "100%");
-        zapnutoClth = true;
-    } else{
-        document.documentElement.style.setProperty("--clth", "0%");
-        zapnutoClth = false;
-
-    }
 })
 sneakersButton.addEventListener("click", function(){
     sneakersDiv.classList.toggle("show-sneakers");
     sneakersDiv.classList.toggle("underline-sneakers");
-    if (!zapnutoSnkrs) {
-        document.documentElement.style.setProperty("--snkrs", "100%");
-        zapnutoSnkrs = true;
-    } else{
-        document.documentElement.style.setProperty("--snkrs", "0%");
-        zapnutoSnkrs = false;
-    }
 })
+
+function changeJordan(shoe) {
+    for (let i = 0; i < jordanDivs.length; i++) {
+                
+    }
+}
